6bbc70a9ddc34efb33671d1f63aedb5734d649f0
NitinGoutham06 <nitingthm@gmail.com>
2022-09-07 11:21:33 +0530

    GM changed code

--- src/test/java/org/sample/ProjectD.java
+++ src/test/java/org/sample/ProjectD.java
@@ -5,8 +5,8 @@ public class ProjectD {
 	public static void main(String[] args) {
 		
 		System.out.println("Test4");
-		System.out.println("Test5");
-		System.out.println("Test6");
+		System.out.println("Test7");
+		System.out.println("Test8");
 
 	}
 }
