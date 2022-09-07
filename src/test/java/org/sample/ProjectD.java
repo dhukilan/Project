a58035710ee143767695aa8205a221315fd50d34
NitinGoutham06 <nitingthm@gmail.com>
2022-09-07 11:15:15 +0530

    GM's code

---
+++ src/test/java/org/sample/ProjectD.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class ProjectD {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
+
+	}
+}
